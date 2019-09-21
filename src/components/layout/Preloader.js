0c58d5c355c3d61c723add9b9d7ba613f6492a06
ndt94 <ndt94hvnh@gmail.com>
2019-09-21 16:28:49 +0700

    Initial commit

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+    return (
+        <div className="progress blue lighter-4">
+            <div className="indetermine blue"></div>
+        </div>
+    );
+};
+
+export default Preloader;
