ef2f72056653f33d42448403668ed0ca204ab91b
ndt94 <ndt94hvnh@gmail.com>
2019-09-24 14:05:31 +0700

    Lesson 77,78,79

--- src/actions/logAction.js
+++ src/actions/logAction.js
@@ -1,11 +1,17 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import {
+    GET_LOGS,
+    SET_LOADING,
+    LOGS_ERROR,
+    ADD_LOG,
+    DELETE_LOG
+} from "./types";
 
 // 1ST - WAY
 // export const getLogs = () => {
 //     return async dispatch => {
 //         setLoading();
 //         const res = await fetch("/logs");
-//         const data = await res.json;
+//         const data = await res.json();
 //         dispatch({
 //             type: GET_LOGS,
 //             payload: data
@@ -13,11 +19,12 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 //     };
 // };
 
+// 2ND WAY TO GET LOG FROM SERVER
 export const getLogs = () => async dispatch => {
     try {
         setLoading();
         const res = await fetch("/logs");
-        const data = await res.json;
+        const data = await res.json();
         dispatch({
             type: GET_LOGS,
             payload: data
@@ -35,4 +42,48 @@ export const setLoading = () => {
     return {
         type: SET_LOADING
     };
+};
+
+// ADD NEW LOG
+export const addLog = log => async dispatch => {
+    try {
+        setLoading();
+        const res = await fetch("/logs", {
+            method: "POST",
+            body: JSON.stringify(log),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await res.json();
+        dispatch({
+            type: ADD_LOG,
+            payload: data
+        });
+    } catch (error) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.data
+        });
+    }
+};
+
+// DELETE LOG FROM SERVER
+export const deleteLog = id => async dispatch => {
+    try {
+        setLoading();
+        await fetch(`/logs/${id}`, {
+            method: "DELETE"
+        });
+
+        dispatch({
+            type: DELETE_LOG,
+            payload: id
+        });
+    } catch (error) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: error.response.data
+        });
+    }
 };
